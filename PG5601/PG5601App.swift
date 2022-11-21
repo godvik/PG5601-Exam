e55b6d07053477215c420b6b2deab83c7feda73d
godvik <32129757+godvik@users.noreply.github.com>
2022-11-21 10:54:30 +0100

    Initial Commit

---
+++ PG5601/PG5601App.swift
@@ -0,0 +1,17 @@
+//
+//  PG5601App.swift
+//  PG5601
+//
+//  Created by Vegard Kristoffer Godvik on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PG5601App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
